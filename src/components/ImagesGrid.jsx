dceb603a0ceb8d9008e4059646db2323c0e003a0
hashamsp36 <hashamsp36@gmail.com>
2023-10-08 17:07:54 -0400

    can click on an image to open up lightbox

--- src/components/ImagesGrid.jsx
+++ src/components/ImagesGrid.jsx
@@ -4,6 +4,10 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+
+import { LightBox } from 'react-lightbox-pack';
+import "react-lightbox-pack/dist/index.css";
+
 const getImages = async (page, perPage) => {
   try {
     const response = await fetch(`${process.env.REACT_APP_UNSPLASH_API}/photos?client_id=${process.env.REACT_APP_CLIENT_ID}&page=${page}&per_page=${perPage}`, {
@@ -24,10 +28,33 @@ function ImagesGrid() {
   const perPage = 10 // 30 aka max limit images per page is probably better to avoid too many api calls
   const [images, setImages] = React.useState([])
 
+  const [toggle, setToggle] = React.useState(false)
+  const [sIndex, setSIndex] = React.useState(0)
+
+    // Handler
+	const  lightBoxHandler  = (state, sIndex) => {
+		setToggle(state);
+		setSIndex(sIndex);
+	};
+
+  const transformDataForLightBox = (images) => {
+    console.log('transforming img data')
+    return images.map((image) => {
+      return {
+        id: image.id,
+        image: image.urls.full,
+        description: image.description,
+        title: image.alt_description,
+      };
+    })  
+  }
+
+
   React.useEffect(() => {
     getImages(page, perPage).then((data) => {
-      console.log('debug data:', data)
-      setImages((prevImages) => [...prevImages, ...data])
+      console.log('load more images:', data)
+      const transformedData = transformDataForLightBox(data)
+      setImages((prevImages) => [...prevImages, ...transformedData])
     }).catch((error) => {
       console.error('Error:', error);
     })
@@ -35,18 +62,34 @@ function ImagesGrid() {
 
 
 
+
+
   return (
     <div className="image-grid">
-      {images.map((image) => (
-        <div className="image-container" key={image.id}>
+      {images.map((image, index) => (
+        <div className="image-container" key={image.id}  onClick={() => {lightBoxHandler(true, index)}}>
           <LazyLoadImage
-            alt={image.alt_description}
+            alt={image.title}
             effect="blur"
-            src={image.urls.full}
-            height={500}/> {/* Note this doesn't crop the image, just temporary height is required else it loads all images at once*/}
+            src={image.image}
+            height={500} // temporary height needed to enable lazy loading
+          />
         </div>
       ))}
       <InfiniteScroll dataLength={images.length} next={() => setPage(page + 1)} hasMore={true} loader={<h4>Loading...</h4>}/>
+
+      <LightBox
+				state={toggle}
+        event={lightBoxHandler}
+        data={images}
+        imageWidth="60vw"
+        imageHeight="70vh"
+        thumbnailHeight={50}
+        thumbnailWidth={50}
+        setImageIndex={setSIndex}
+        imageIndex={sIndex}
+			/>
+
     </div>
   );
 }
